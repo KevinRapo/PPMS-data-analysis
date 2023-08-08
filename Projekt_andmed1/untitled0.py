e2faccead54e5799c4f9cf795d8987beb27b1336
KevinRapo <141725557+KevinRapo@users.noreply.github.com>
2023-08-08 13:19:53 +0300

    Add files via upload

---
+++ Projekt_andmed1/untitled0.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  6 13:33:09 2023
+
+@author: Kevin
+"""
+nr = [34, 42]
+# Creating an empty dictionary
+my_dict = {}
+
+# Adding key-value pairs to the dictionary
+my_dict["name"] = "John"
+my_dict["age"] = 30
+my_dict["city"] = "New York"
+
+# Printing the dictionary
+print(my_dict)
+
+adre = "teekond"
+
+raamat = dict(nr = adre)
